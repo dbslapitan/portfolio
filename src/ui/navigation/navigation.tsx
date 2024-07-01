584adcdd9c527f47cee2198ce1cf232a6f562d0a
dbslapitan <dbslapitan@gmail.com>
2024-07-01 03:28:19 -0400

    finish nav design

--- src/ui/navigation/navigation.tsx
+++ src/ui/navigation/navigation.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import style from './navigation.module.scss';
+import Link from 'next/link';
 
 export default function Navigation(){
 
@@ -14,6 +15,23 @@ export default function Navigation(){
     return(
         <div className={`${style['navigation']}`}>
             <button className={`${style['navigation__toggle']} ${isOpen ? style['navigation__toggle--open'] : style['navigation__toggle--close']}`} onClick={clickHandler}></button>
+            <ul className={`${style['navigation__links']} ${ isOpen ? '' : style['navigation__links--hide']}`}>
+                <li className={`${style['navigation__item']}`}>
+                    <Link className={`${style['navigation__link']}`} href={`#`}>
+                        Home
+                    </Link>
+                </li>
+                <li className={`${style['navigation__item']}`}>
+                    <Link className={`${style['navigation__link']}`} href={`#`}>
+                        About
+                    </Link>
+                </li>
+                <li className={`${style['navigation__item']}`}>
+                    <Link className={`${style['navigation__link']}`} href={`#`}>
+                        Projects
+                    </Link>
+                </li>
+            </ul>
         </div>
     );
 }
