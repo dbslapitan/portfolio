43c7433983da6c7fc9e04b7b355299f9482f174d
dbslapitan <dbslapitan@gmail.com>
2024-07-01 05:59:07 -0400

    start on about page

--- src/ui/navigation/navigation.tsx
+++ src/ui/navigation/navigation.tsx
@@ -17,12 +17,12 @@ export default function Navigation(){
             <button className={`${style['navigation__toggle']} ${isOpen ? style['navigation__toggle--open'] : style['navigation__toggle--close']}`} onClick={clickHandler}></button>
             <ul className={`${style['navigation__links']} ${ isOpen ? '' : style['navigation__links--hide']}`}>
                 <li className={`${style['navigation__item']}`}>
-                    <Link className={`${style['navigation__link']}`} href={`#home`}>
+                    <Link className={`${style['navigation__link']}`} href={`#home`} onClick={clickHandler}>
                         Home
                     </Link>
                 </li>
                 <li className={`${style['navigation__item']}`}>
-                    <Link className={`${style['navigation__link']}`} href={`#about`}>
+                    <Link className={`${style['navigation__link']}`} href={`#about`} onClick={clickHandler}>
                         About
                     </Link>
                 </li>
